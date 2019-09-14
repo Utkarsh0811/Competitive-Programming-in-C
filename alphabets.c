cb451f01469b8538a6710a8609c3d7323caea085
Utkarsh0811 <42350006+Utkarsh0811@users.noreply.github.com>
2019-09-15 00:14:13 +0530

    Update alphabets.c

--- alphabets.c
+++ alphabets.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 
 int main()
-{
+{   // program to print alphabets from A to Z
     char ch;
     int a;
-    for(ch='A';ch<='Z';ch++){
+    // Method-1
+    for(ch='A';ch<='Z';ch++)
+    {
         printf("%c",ch);
     }
     printf("\n");
+    // Method-2
     for(a=65;a<=90;a++)
       printf("%c",a);
 }
